ced8d1319071f3047fba860a000f9e8cb32f3b79
Giorgio <Giorgio@Clementines-MacBook-Pro.local>
2019-09-16 21:57:55 +0200

    update1

--- index.js
+++ index.js
@@ -1,12 +1,80 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
+
+mongoose.set("useCreateIndex", true); // what is this doing??
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+    return Recipe.deleteMany();
+  })
+  .then(result => {
+    return Recipe.insertMany(data);
+  })
 
+  .then(result => {
+    console.log("Success", result); // how to console.log only the title??
+  })
+
+  .then(result => {
+    // how to console.log the success message??
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+
+      {
+        duration: 100
+      },
+      {
+        new: true
+      }
+    );
+  })
+
+  .then(result => {
+    return Recipe.findOneAndDelete({ title: "Carrot Cake" });
+    // how to console.log the success message??
+  })
+
+  .then(result => {
+    return Recipe.create(
+      {
+        title: "Spaghetti alla Carbonara",
+        level: "Easy Peasy",
+        ingredients: [
+          "Spaghetti",
+          "eggs",
+          "Pepper",
+          "Salt",
+          "Parmigiano",
+          "Bacon"
+        ],
+        cuisine: "Italian",
+        dishType: "Dish",
+        image:
+          "https://images.media-allrecipes.com/userphotos/720x405/3605684.jpg",
+        duration: 40,
+        creator: "Giorgio"
+      },
+      function(err, Recipe) {
+        if (err) {
+          console.log("An error happened:", err);
+        } else {
+          console.log("The Recipe is saved and its value is: ", Recipe);
+        }
+      }
+    );
+  })
+  // .catch(err => {
+  //   console.error("Error connecting to mongo", err);
+  // });
+
+  .then(() => {
+    return mongoose.connection.close();
+  })
+  .then(() => console.log("conn closed"))
+
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: { type: String, required: true, unique: true },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: Array,
+  cuisine: { type: String, required: true },
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+  },
+  image: String,
+  duration: Number,
+  creator: String,
+  created: { type: Date, default: Date.now }
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;
